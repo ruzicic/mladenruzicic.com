dee826155557047b43a03ac3f88d56dec28f0535
Mladen Ruzicic <ruzicic@gmail.com>
2021-10-31 17:48:52 +0100

    Add fathom analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
+import { useAnalytics } from 'hooks/useAnalytics'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useAnalytics()
+
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
