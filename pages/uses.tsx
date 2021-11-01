f042e2035938d005374e4726836a5c398be3343c
Mladen Ruzicic <ruzicic@gmail.com>
2021-11-02 00:01:07 +0100

    Fix uses page header

--- pages/uses.tsx
+++ pages/uses.tsx
@@ -17,15 +17,16 @@ export default function Uses({ post }: { post: Post }) {
       description="Here's what tech I'm using day to day"
     >
       <article className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
+        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
+          {post.title}
+        </h1>
         <div className="prose dark:prose-dark w-full">
-          <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
-            {post.title}
-          </h1>
           <p className="text-gray-700 dark:text-gray-300 mt-2 mb-8">
             {post.excerpt}
           </p>
           <p className="text-gray-700 dark:text-gray-300 mt-2 mb-8">
-            <strong>Last update:</strong> <DateFormatted dateString={post.date} />
+            <strong>Last update:</strong>{' '}
+            <DateFormatted dateString={post.date} />
           </p>
           <PostBody content={post.content} />
         </div>
