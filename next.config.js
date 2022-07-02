cd791bd9f91fc76f7df42a2406d5b537fb123a6c
Mladen Ruzicic <ruzicic@gmail.com>
2022-07-02 15:23:13 +0200

    Revert url redirect

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/pitch-deck',
-        destination: 'https://docs.google.com/presentation/d/e/2PACX-1vSpzAy_XZyKrco4k47P5eaDnYqB36VF0L2T2WbGLLTCl9nXVNb8H7ZovXgSVpdoXHCzBQurgPWbRp54/pub?start=true&loop=false&delayms=60000',
-        permanent: false,
-      },
-    ]
-  },
 }
