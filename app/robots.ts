3579ffe7b6271f0d0b66e6685902b360a06a640b
Mladen Ruzicic <ruzicic@gmail.com>
2023-07-06 14:43:39 +0200

    Add sitemap and robots

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+      },
+    ],
+    sitemap: 'https://mladenruzicic.com/sitemap.xml',
+    host: 'https://mladenruzicic.com',
+  }
+}
---
+++ app/sitemap.ts
@@ -0,0 +1,10 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const routes = ['', '/about', '/projects', '/uses'].map((route) => ({
+    url: `https://mladenruzicic.com/${route}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }))
+
+  return [...routes]
+}
