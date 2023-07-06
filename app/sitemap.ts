81924e49bf0bb52f3b4a1976fe37481c523b578f
Mladen Ruzicic <ruzicic@gmail.com>
2023-07-06 14:50:25 +0200

    fix double dash in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const routes = ['', '/about', '/projects', '/uses'].map((route) => ({
-    url: `https://mladenruzicic.com/${route}`,
+    url: `https://mladenruzicic.com${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
 
